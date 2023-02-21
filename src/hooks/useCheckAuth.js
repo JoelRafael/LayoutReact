a6f7c260cd9bdd11cfa59a847491a0d881d450e6
Joel Paredes <j.paredes@gruporamos.com>
2023-02-21 18:02:21 -0400

    Se agrego las notas dentro del slice ahora falta desplegarla en el seedvar

--- src/hooks/useCheckAuth.js
+++ src/hooks/useCheckAuth.js
@@ -3,21 +3,20 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout } from "../store/auth/authSlice";
 import { FirebaseAuth } from "../firebase/config";
+import { startLoadingNotes } from "../store/journal/thunks";
 
-export const useCheckAuth = () =>{
-    const { status } = useSelector((state) => state.auth);
-    const dispatch = useDispatch();
+export const useCheckAuth = () => {
+  const { status } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      if (!user) return dispatch(logout());
+      const { uid, email, photoURL, displayName } = user;
+      dispatch(login({ uid, email, displayName, photoURL }));
+      dispatch(startLoadingNotes());
+    });
+  }, []);
 
-      onAuthStateChanged(FirebaseAuth, async (user) => {
-        if (!user) return dispatch(logout());
-        const { uid, email, photoURL, displayName } = user;
-        dispatch(login({ uid, email, displayName, photoURL }));
-      });
-
-    }, []);
-    
-    return status
-    
-}
+  return status;
+};
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,28 +1,35 @@
-
-import { JournalLayout } from "../layout/JournalLayout"
-import { NothingSelectedViews, NoteView } from "../views"
-import { IconButton } from '@mui/material';
-import { AddOutlined } from '@mui/icons-material';
-
+import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedViews, NoteView } from "../views";
+import { IconButton } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { startNewNote } from "../../store/journal/thunks";
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+  const { isSaving, active } = useSelector((state) => state.journal);
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  };
   return (
     <JournalLayout>
-      <NothingSelectedViews/>  
-     {/* <NoteView/>  */}
+      {!!active ? <NoteView /> : <NothingSelectedViews />}
+
       <IconButton
-        size='large'
+        size="large"
+        disabled={isSaving}
         sx={{
-          color: 'white',
-          backgroundColor: 'error.main',
-          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
-          position: 'fixed',
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
           right: 50,
-          bottom: 50
+          bottom: 50,
         }}
+        onClick={onClickNewNote}
       >
         <AddOutlined sx={{ fontSize: 30 }} />
       </IconButton>
     </JournalLayout>
-  )
-}
+  );
+};
