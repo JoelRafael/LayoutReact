110f96fe45e2ca67ea484094ebeae609b5e8e027
Joel Paredes <j.paredes@gruporamos.com>
2023-02-28 17:17:49 -0400

    add sweetalert2

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,31 +1,41 @@
 import { SaveOutlined } from "@mui/icons-material";
 import { Grid, Typography, Button, TextField } from "@mui/material";
 import { ImageGallery } from "../components/ImageGallery";
-import {useForm} from "../../hooks/useForm";
+import { useForm } from "../../hooks/useForm";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useMemo } from "react";
-import {setActiveNote} from "../../store/journal/journaSlice";
+import { setActiveNote } from "../../store/journal/journaSlice";
 import { startSaveNote } from "../../store/journal/thunks";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.css";
 export const NoteView = () => {
-
   const dispatch = useDispatch();
 
-  const  { active:note } = useSelector(state => state.journal)
+  const {
+    active: note,
+    messageSaved,
+    isSaving,
+  } = useSelector((state) => state.journal);
 
-  const {body, title, date, onInputChange, formState} = useForm(note);
+  const { body, title, date, onInputChange, formState } = useForm(note);
 
-  const dateString = useMemo(()=>{
+  const dateString = useMemo(() => {
     const newDate = new Date(date);
     return newDate.toUTCString();
-  },[date])
+  }, [date]);
 
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(setActiveNote(formState));
-  },[formState])
+  }, [formState]);
 
-  const onSavetNote = () =>{
+  useEffect(() => {
+    if (messageSaved.length > 0) {
+      Swal.fire("Nota actualizada", messageSaved, "success");
+    }
+  }, [messageSaved]);
+  const onSavetNote = () => {
     dispatch(startSaveNote());
-  }
+  };
 
   return (
     <Grid
@@ -38,15 +48,16 @@ export const NoteView = () => {
     >
       <Grid item>
         <Typography fontSize={39} fontWeight="light">
-         {dateString}
+          {dateString}
         </Typography>
       </Grid>
 
       <Grid item>
-        <Button 
-        onClick={onSavetNote}
-        color="primary" 
-        sx={{ padding: 2 }}
+        <Button
+          disabled={isSaving}
+          onClick={onSavetNote}
+          color="primary"
+          sx={{ padding: 2 }}
         >
           <SaveOutlined sx={{ fontSize: 30, mr: 1 }}></SaveOutlined>
           Guardar
@@ -60,7 +71,7 @@ export const NoteView = () => {
           placeholder="Ingrese un titulo"
           label="Titulo"
           sx={{ border: "none", mb: 1 }}
-          name='title'
+          name="title"
           value={title}
           onChange={onInputChange}
         ></TextField>
@@ -72,7 +83,7 @@ export const NoteView = () => {
           multiline
           placeholder="Que sucedio hoy?"
           minRows={5}
-          name='body'
+          name="body"
           value={body}
           onChange={onInputChange}
         ></TextField>
